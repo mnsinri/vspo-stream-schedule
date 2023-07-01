92b1a343ff4c044bfd4f8724d944b9b563cba90b
mnsinri <khj.mkyohei727@gmail.com>
2023-07-01 20:28:20 +0900

    change desktop ui

--- src/components/StreamingTable.tsx
+++ src/components/StreamingTable.tsx
@@ -1,35 +1,69 @@
-import React from "react";
+import React, { useLayoutEffect, useMemo, useRef, useState } from "react";
 import styled from "styled-components";
-import { animated } from "@react-spring/web";
 import { StreamingTableProps } from "../types";
 import { StreamingCard } from "./card";
+import { useWindowSize } from "../hooks";
 import { theme } from "../theme";
 
-const Container = styled(animated.div)`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, 160px);
-  grid-gap: 20px 0px;
+const Container = styled.div<{ height: number }>`
+  min-height: ${(p) => p.height}px;
+  display: flex;
+  flex-direction: row;
   justify-content: space-between;
+`;
+
+const FlexBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 
   ${theme.breakpoints.mediaQueries.md`
-    grid-template-columns: repeat(auto-fill, 320px);
+    gap: 40px;
   `}
 `;
 
-export const StreamingTable: React.FC<StreamingTableProps> = ({
-  streams,
-  ...props
-}) => {
-  const sortedStreams = streams.sort((a, b) =>
-    a.startAt + a.name > b.startAt + b.name ? 1 : -1
+export const StreamingTable: React.FC<StreamingTableProps> = ({ streams }) => {
+  const container = useRef<HTMLDivElement>(null!);
+  const { x, isMobile } = useWindowSize();
+  const [rowNum, setRowNum] = useState<number>(0);
+
+  const sortedStreams = useMemo(
+    () =>
+      streams.sort((a, b) =>
+        a.startAt + a.name > b.startAt + b.name ? 1 : -1
+      ),
+    [streams]
   );
-  const checkStream = () => streams.length > 0;
+
+  useLayoutEffect(() => {
+    setRowNum(
+      Math.floor((container.current.offsetWidth ?? 0) / (isMobile ? 160 : 320))
+    );
+  }, [x]);
+
+  const height = useMemo(() => {
+    const columnNum = Math.ceil(streams.length / rowNum);
+
+    return (
+      columnNum * (isMobile ? 90 : 180) +
+      (columnNum - 1) * 40 +
+      (isMobile ? 30 : 60)
+    );
+  }, [rowNum]);
+
+  const streamsMatrix = useMemo(
+    () =>
+      [...Array(rowNum)].map((_, i) =>
+        sortedStreams.filter((_, j) => j % rowNum === i)
+      ),
+    [rowNum]
+  );
 
   return (
-    <Container {...props}>
-      {checkStream() ? (
-        sortedStreams.map((s) => {
-          return (
+    <Container ref={container} height={height}>
+      {streamsMatrix.map((sts, i) => (
+        <FlexBox key={i}>
+          {sts.map((s) => (
             <StreamingCard
               key={s.id}
               title={s.title}
@@ -40,11 +74,9 @@ export const StreamingTable: React.FC<StreamingTableProps> = ({
               url={s.url}
               startAt={s.startAt}
             />
-          );
-        })
-      ) : (
-        <h1>No data GG</h1>
-      )}
+          ))}
+        </FlexBox>
+      ))}
     </Container>
   );
 };
