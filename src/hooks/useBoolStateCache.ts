3efe0d0c021f47ab7ad967f492b750cfa52a59ea
mnsinri <khj.mkyohei727@gmail.com>
2023-09-03 04:11:54 +0900

    fix: initialize in the useBoolStateCache

--- src/hooks/useBoolStateCache.ts
+++ src/hooks/useBoolStateCache.ts
@@ -2,7 +2,10 @@ import { useEffect, useState } from "react";
 
 export const useBoolStateCache = (key: string, initState: boolean) => {
   const [value, setter] = useState<boolean>(
-    localStorage.getItem(key)?.toLocaleLowerCase() === "true" ?? initState
+    (() => {
+      const item = localStorage.getItem(key);
+      return item == null ? initState : item.toLocaleLowerCase() === "true";
+    })()
   );
 
   useEffect(() => {
