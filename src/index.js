bcc1ad97891c33222077d923f48faeddf7787185
mnsinri <khj.mkyohei727@gmail.com>
2024-10-14 22:03:49 +0900

    delete: ついで

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { App } from "./app";
+import { App } from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
